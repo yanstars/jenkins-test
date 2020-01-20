eb38011c48ed635e6226c24154d808d323f457ed
yanstars <835975379@qq.com>
2020-01-20 11:20:26 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>im a {{data}}</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            data: ' test '
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
